aa606bec750a615d5e946b4e0b2067cc3945553b
Andrew Harvey <andrew.rossharvey@gmail.com>
2012-12-13 22:48:01 +0000

    Use GLEW library for binding to OpenGL API

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 
+#include <GL/glew.h>
+
+#include "SDL.h"
+
 int
 main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
+
+	SDL_Surface* pSurface = SDL_SetVideoMode(
+								800, 600, 32,
+								SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_OPENGL);
+	SDL_WM_SetCaption("Zillion", 0);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+
+	std::cout << vertexBuffer << std::endl;
+
+	SDL_Event windowEvent;
+	while(true)
+	{
+		if(SDL_PollEvent(&windowEvent))
+		{
+			if(windowEvent.type == SDL_QUIT)
+				break;
+
+			if(windowEvent.type == SDL_KEYUP &&
+				windowEvent.key.keysym.sym == SDLK_ESCAPE)
+				break;
+		}
+
+		SDL_GL_SwapBuffers();
+	}
+
+	SDL_Quit();
+
 	return 0;
 }
